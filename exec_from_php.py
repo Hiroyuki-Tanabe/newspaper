1c2c19fa097a7c595372fa19026cd1f48b1e86f0
Hiroyuki-Tanabe <51527079+Hiroyuki-Tanabe@users.noreply.github.com>
2020-12-26 20:30:15 +0900

    Add files via upload

--- exec_from_php.py
+++ exec_from_php.py
@@ -4,7 +4,7 @@ import json
 def main():
 
     url = "https://slack.com/api/conversations.history"
-    token = "xoxp-1000722060884-1003387710614-1597352462883-abde089591db2d4b6b1cd6c0f3e5be4a"
+    token = "xoxp-1000722060884-1003387710614-1599905019139-5f9fb3518f61faa7d18eb8610a30989a"
     channel_id = "C0100M822JY"
 
     payload = {
