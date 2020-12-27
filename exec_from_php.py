9fe86a1f3e5987e3d532e951d0adb660244c39c0
Hiroyuki-Tanabe <51527079+Hiroyuki-Tanabe@users.noreply.github.com>
2020-12-27 13:55:07 +0900

    Update exec_from_php.py

--- exec_from_php.py
+++ exec_from_php.py
@@ -4,7 +4,7 @@ import json
 def main():
 
     url = "https://slack.com/api/conversations.history"
-    token = "xoxp-1000722060884-1003387710614-1599905019139-5f9fb3518f61faa7d18eb8610a30989a"
+    token = "xxx"
     channel_id = "C0100M822JY"
 
     payload = {
